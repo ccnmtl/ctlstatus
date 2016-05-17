6688484b1fc2b21f7ab1c4fe2d41542a86fcc808
Anders Pearson <anders@columbia.edu>
2016-05-17 16:05:29 +0100

    define some basic types

--- ctlstatus.go
+++ ctlstatus.go
@@ -3,10 +3,38 @@ package ctlstatus
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"google.golang.org/appengine/datastore"
 )
+
+type Incident struct {
+	Status      string
+	Start       time.Time
+	End         time.Time
+	Summary     string
+	Description string
+}
+
+type Update struct {
+	Incident  *datastore.Key
+	Status    string
+	timestamp time.Time
+	comment   string
+}
+
+type MaintenanceWindow struct {
+	Summary     string
+	Start       time.Time
+	End         time.Time
+	Description string
+}
 
 func init() {
 	http.HandleFunc("/", indexHandler)
+	//	http.HandleFunc("/incident/new", newIncidentHandler)
+	//	http.HandleFunc("/incident/", incidentDetailHandler)
+	//	http.HandleFunc("/maintenance/new", newIncidentHandler)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
