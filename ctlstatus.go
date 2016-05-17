691bee7c2223914d6c2fed6135bed3c9a0d42a73
Anders Pearson <anders@columbia.edu>
2016-05-17 16:47:14 +0100

    add a new incident and display recent incidents

--- ctlstatus.go
+++ ctlstatus.go
@@ -5,7 +5,8 @@ import (
 	"text/template"
 	"time"
 
-	"google.golang.org/appengine/datastore"
+	"appengine"
+	"appengine/datastore"
 )
 
 type Incident struct {
@@ -32,7 +33,7 @@ type MaintenanceWindow struct {
 
 func init() {
 	http.HandleFunc("/", indexHandler)
-	//	http.HandleFunc("/incident/new", newIncidentHandler)
+	http.HandleFunc("/incident/new", newIncidentHandler)
 	//	http.HandleFunc("/incident/", incidentDetailHandler)
 	//	http.HandleFunc("/maintenance/new", newIncidentHandler)
 }
@@ -41,8 +42,33 @@ var indexTemplate = template.Must(template.ParseFiles("templates/base.html",
 	"templates/index.html"))
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	q := datastore.NewQuery("Incident").Order("-End").Limit(10)
+	incidents := make([]Incident, 0, 10)
+	if _, err := q.GetAll(ctx, &incidents); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	tc := make(map[string]interface{})
+	tc["incidents"] = incidents
 	if err := indexTemplate.Execute(w, tc); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func newIncidentHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	incident := &Incident{
+		Status:      "investigating",
+		Start:       time.Now(),
+		End:         time.Now(),
+		Summary:     r.FormValue("summary"),
+		Description: r.FormValue("description"),
+	}
+	key := datastore.NewIncompleteKey(ctx, "Incident", nil)
+	if _, err := datastore.Put(ctx, key, incident); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/", http.StatusFound)
 }
