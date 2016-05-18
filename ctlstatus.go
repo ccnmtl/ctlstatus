75a64633323f7cc4e73242b7ff45bd6bbb18aeb6
Anders Pearson <anders@columbia.edu>
2016-05-18 19:44:03 +0100

    split update out to its own file

--- ctlstatus.go
+++ ctlstatus.go
@@ -12,21 +12,6 @@ import (
 	"appengine/user"
 )
 
-type Update struct {
-	Incident  *datastore.Key
-	Status    string
-	Timestamp time.Time
-	Comment   string
-}
-
-func (u Update) BootstrapClass() string {
-	return BootstrapClassFromStatus(u.Status)
-}
-
-func (u Update) DisplayTimestamp() string {
-	return u.Timestamp.Format("Mon Jan 2 15:04 2006")
-}
-
 type MaintenanceWindow struct {
 	Key         string
 	Summary     string
---
+++ update.go
@@ -0,0 +1,22 @@
+package ctlstatus
+
+import (
+	"time"
+
+	"appengine/datastore"
+)
+
+type Update struct {
+	Incident  *datastore.Key
+	Status    string
+	Timestamp time.Time
+	Comment   string
+}
+
+func (u Update) BootstrapClass() string {
+	return BootstrapClassFromStatus(u.Status)
+}
+
+func (u Update) DisplayTimestamp() string {
+	return u.Timestamp.Format("Mon Jan 2 15:04 2006")
+}
