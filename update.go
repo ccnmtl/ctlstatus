485591a7fdcbd01bd9a69943c2ec9491a58a8447
Anders Pearson <anders@columbia.edu>
2016-06-02 10:45:01 +0100

    markdown for updates as well

--- update.go
+++ update.go
@@ -1,7 +1,10 @@
 package ctlstatus
 
 import (
+	"html/template"
 	"time"
+
+	"github.com/russross/blackfriday"
 
 	"appengine/datastore"
 )
@@ -19,4 +22,8 @@ func (u Update) BootstrapClass() string {
 
 func (u Update) DisplayTimestamp() string {
 	return u.Timestamp.In(NYC).Format("Mon Jan 2 15:04")
+}
+
+func (u Update) RenderComment() template.HTML {
+	return template.HTML(string(blackfriday.MarkdownCommon([]byte(u.Comment))))
 }
